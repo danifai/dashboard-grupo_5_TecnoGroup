e0229ddc1c5b9a57f84e71050864716024b330bb
danifai <daniel.fai89@gmail.com>
2022-09-05 00:43:31 -0300

    sprint mínimo hecho

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,21 +2,30 @@ import React from 'react';
 
 import MovieList from './MovieList';
 
-// function Movie(){
-//     return(
-
-//     )
-// }
-
 class Movie extends React.Component {
 	state = {
-		moviesList: [],
+		productsList: [],
+	}
+
+	apiProductsResponse() {
+		fetch('http://localhost:3002/api/products')
+			.then(response => response.json())
+			.then(products => {
+				this.setState({
+					productsList: products.products,
+				});
+			})
+			.catch(err => console.log(err));
+	}
+
+	componentDidMount() {
+		this.apiProductsResponse();
 	}
 
 	render() {
 		return (<React.Fragment>
 			{/*<!-- PRODUCTS LIST -->*/}
-			<h1 className="h3 mb-2 text-gray-800">All the movies in the Database</h1>
+			<h1 className="h3 mb-2 text-gray-800">All products in the Database</h1>
 
 			{/*<!-- DataTales Example -->*/}
 			<div className="card shadow mb-4">
@@ -26,36 +35,25 @@ class Movie extends React.Component {
 							<thead>
 								<tr>
 									<th>Id</th>
-									<th>Titulo</th>
-									<th>Calificación</th>
-									<th>Premios</th>
-									<th>Duración</th>
+									<th>Nombre</th>
+									<th>Descripción</th>
 								</tr>
 							</thead>
 							<tfoot>
 								<tr>
-									<th>Id</th>
-									<th>Titulo</th>
-									<th>Calificación</th>
-									<th>Premios</th>
-									<th>Duración</th>
+
 								</tr>
 							</tfoot>
 							<tbody>
-								<tr>
-									<td>01</td>
-									<td>Reto al destino</td>
-									<td>20</td>
-									<td>15</td>
-									<td>120</td>
-								</tr>
-								<tr>
-									<td>02</td>
-									<td>La caida del halcon negro</td>
-									<td>10</td>
-									<td>18</td>
-									<td>240</td>
-								</tr>
+								{
+									this.state.productsList.map((product, index) =>
+										<tr>
+											<td>{product.id}</td>
+											<td>{product.name}</td>
+											<td>{product.description}</td>
+										</tr>
+									)
+								}
 							</tbody>
 						</table>
 					</div>
